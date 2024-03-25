7823317d41c21863fd1d932bb438104952dabe6c
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-03-25 11:45:31 +0300

    create functions

---
+++ index.js
@@ -0,0 +1,21 @@
+const divide = function (a, b) {
+    return a / b;
+}
+let a = 2000;
+let b = 100
+
+divide(a,b);
+
+const square = (x) => x * x;
+let x = 2;
+square(x);
+
+const add = (c, d) => c + d;
+let c = 3;
+let d = 4;
+add(c,d);
+
+
+
+
+
